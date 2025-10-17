a7f1eb84973aca2bc6f655117f95a045afbacc53
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-16 23:05:44 -0700

    Update game.py
    Created import list from external files.

--- src/game.py
+++ src/game.py
@@ -7,3 +7,22 @@
 # * Replay option
 # * Maintain the structure of each round/loop
 
+from __future__ import annotation
+from pathlib import Path
+
+from .logic import(
+  GameState,
+  init_state,
+  masked_word
+  already_guessed,
+  apply_guess
+  is_win,
+  is_lose,
+)
+
+from .words import load_words, choose_answer
+
+def _prompt_guess() => str:
+  # This evaluates the player input. If valid, process. If invalid prompt & return to input. 
+
+```
