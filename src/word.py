49d698fd885d356952fd392a350c0b596db52e3b
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-16 19:20:23 -0700

    Create word.py
    Created file to handle word bank. Outlined planned functions to define.

---
+++ src/word.py
@@ -0,0 +1,10 @@
+### This file contains:
+### * A default word bank.
+### * Defines function for loading a new word bank.
+### * Defines a function to select a random word from the bank.
+
+```python
+
+
+
+```
