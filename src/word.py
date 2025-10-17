8c923b38a4edbecef53deeb6c44895d983863115
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-16 19:34:27 -0700

    Update word.py
    Outlined function to load word file from path, and default word bank.

--- src/word.py
+++ src/word.py
@@ -4,7 +4,18 @@
 ### * Defines a function to select a random word from the bank.
 
 ```python
+__from__ future import annotations
+import random
+from pathlib import Path
 
+_DEFAULT_WORDS: list[str] = [ # put default list here, each word in quotes & separated by comma
 
+]
+
+def load_words (path: str | None = None) -> list[str]:
+  # This loads words from a .txt file if indicated.
+  # If no data file is provided, the function will fall to default word bank.
+  # Data is sanitized to only accept alphabetic characters in lower case.
+  
 
 ```
