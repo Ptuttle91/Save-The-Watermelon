49e20289f2905e70fa891fb4236a4e7c7fb6e806
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-17 12:52:54 -0700

    Update test_logic.py
    Outlined goals for testing

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,1 +1,10 @@
-
+Testing of game logic
+Goals:
+* test masking function
+* test correct guess function
+* test incorrect guess function
+* test duplicate input with no penalyu function
+* test win state
+* test lose state 
+* test default word bank loading
+* test rng functionality
