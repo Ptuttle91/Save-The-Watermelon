b99ebc32e88da372ac3a851bb22103a2c7ec3d6d
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-17 01:33:39 -0700

    Update logic.py
    Fixed syntax errors, fixed incomplete function call in apply_guess.

--- src/logic.py
+++ src/logic.py
@@ -7,19 +7,18 @@ from dataclasses import dataclass, field
 
 @dataclass
 class GameState:
-    ## This is the container for all mutable game states.
-    ## It holds the following attributes:
-    #### - answer: The hidden word
-    #### - correct_tries: A mask/unmasked with prepositioning for correctly guessed letters.
-    #### - guessed: This is the bank of all letters that have been guessed
-    #### - slices: The amount of remaining lives.
-    #### - total_slices: The starting lives value, modify for potential difficulty adjustments. Based on 'slices'.
-
-answer: str
-correct_tries: list[str]
-guessed: set[str] = field(default_factory=set)
-slices: int = 7
-total_slices: int = 7
+# This is the container for all mutable game states.
+# It holds the following attributes:
+# - answer: The hidden word
+# - correct_tries: A mask/unmasked with prepositioning for correctly guessed letters.
+# - guessed: This is the bank of all letters that have been guessed
+# - slices: The amount of remaining lives.
+# - total_slices: The starting lives value, modify for potential difficulty adjustments. Based on 'slices'.
+    answer: str
+    correct_tries: list[str]
+    guessed: set[str] = field(default_factory=set)
+    slices: int = 7
+    total_slices: int = 7
 
 def init_state(answer: str, slices: int = 7) -> GameState:
   ## This will initialize a new 'GameState' with the masked secret word
@@ -31,17 +30,19 @@ def masked_word(state: GameState) -> str:
   return " ".join(state.correct_tries)
 
 def already_guessed(state: GameState, letter: str) -> bool:
-  #This checks for duplicate guesses
+    #This checks for duplicate guesses
+    return letter in state.guessed
+
 
 def apply_guess(state: GameState, letter: str) -> None:
   # This applies the guessed letter to the secret word, and handles accounting to correct/duplicate/wrong
-  if letter in state.guessed:
-    return
-  state.guessed.add(letter)
+    if letter in state.guessed:
+        return
+    state.guessed.add(letter)
     if letter in state.answer:
         for i, ch in enumerate(state.answer):
-      if ch == letter:
-        state.correct_tries[i] = letter
+            if ch == letter:
+                state.correct_tries[i] = letter
     else:
         state.slices -= 1
 
