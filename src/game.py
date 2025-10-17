e53c0f2651b3196aa5b88e48c54f54e3184fcd00
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-17 00:01:26 -0700

    Update game.py
    Created player input function with data validation. Declared essential variables.

--- src/game.py
+++ src/game.py
@@ -19,10 +19,25 @@ from .logic import(
   is_win,
   is_lose,
 )
-
 from .words import load_words, choose_answer
 
+DEFAULT_SLICES = 7
+WORD_FILE = "data/words.txt"
+# words.py should catch if text file 
+
 def _prompt_guess() => str:
-  # This evaluates the player input. If valid, process. If invalid prompt & return to input. 
+  # This evaluates the player input. If valid, process. If invalid prompt & return to input.
+  while true:
+    raw = input ("Enter a letter to guess!: ").strip()
+    if len(eaw) !=1:
+      print("Error: Invalid Input. Please enter one character at a time.")
+      continue
+    ch = raw.lower()
+    if not ("a" <= ch <= "z"):
+      print("Error: Invalid Input. Only letters A-Z can be accepted.")
+      continue
+    return ch
+
+
 
 ```
