dc197c24008c0564733b5727b68421162ffdbf75
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-16 21:00:16 -0700

    Update word.py
    Populated the _default_words word bank, built word file function

--- src/word.py
+++ src/word.py
@@ -8,14 +8,26 @@ __from__ future import annotations
 import random
 from pathlib import Path
 
-_DEFAULT_WORDS: list[str] = [ # put default list here, each word in quotes & separated by comma
-
+_DEFAULT_WORDS: list[str] = [ 
+  "snail", "watermelon", "cat", "banana", "wolverine", "orange", "badger", "grape", "antelope", "calamansi",                            
 ]
 
 def load_words (path: str | None = None) -> list[str]:
   # This loads words from a .txt file if indicated.
   # If no data file is provided, the function will fall to default word bank.
   # Data is sanitized to only accept alphabetic characters in lower case.
-  
+  words: list[str] = []
+  if path:
+      p = path(path)
+        if p.exists():
+          for line in p.read_text(encoding="etf-8").splitlines():
+            w = line.strip()
+            if not w or w.startswith("#"):
+              continue
+            if w.isalpha() and w.islower():
+              words.append(w)
+  if not words:
+    words = list(_DEFAULT_WORDS)
+  return words
 
 ```
