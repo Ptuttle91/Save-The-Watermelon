dc136d502846bf2bc5f15b46e29226a70ad30d51
Ptuttle91 <patuttle2.0@gmail.com>
2025-10-16 21:52:29 -0700

    Create game.py
    Created game.py file, outlined functions to be defined

---
+++ src/game.py
@@ -0,0 +1,9 @@
+```python
+# This is the main game file. It will connect the logic and word files and provide the prompts and player outputs.
+# It will hold the following functions:
+# * Initiate play/start game
+# * Guess input
+# * Connect with word.py to path to word file 
+# * Replay option
+# * Maintain the structure of each round/loop
+
